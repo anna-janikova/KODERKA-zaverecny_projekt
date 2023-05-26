dc9b8b2d472f4aff47835bd25c9924e84f4ee42e
Anna Janíková <anna-janikova@post.cz>
2023-05-26 12:32:27 +0200

    zakomentové eleventyConfig.addPassthroughCopy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("obrazy");
-    eleventyConfig.addPassthroughCopy("detaily-obrazu");
+    //eleventyConfig.addPassthroughCopy("detaily-obrazu");
     eleventyConfig.addPassthroughCopy("photos");
     eleventyConfig.addPassthroughCopy("css");
 
